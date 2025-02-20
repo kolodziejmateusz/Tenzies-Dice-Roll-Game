6208f6646fe9edaf665932f5e843fe56570bc701
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-02-20 18:32:20 +0100

    Tenzies: Generate 10 random numbers

--- Tenzies-Capstone-Project/src/App.jsx
+++ Tenzies-Capstone-Project/src/App.jsx
@@ -4,21 +4,24 @@ import "./App.css";
 /**
  * Challenge:
  *
- * - Create a Die component that takes a `value` prop. Should
- *   render a button with that value displayed.
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
+ * Write a function (generateAllNewDice) that returns an array
+ * of 10 random numbers between 1-6 inclusive.
+ *
+ * Log the array of numbers to the console for now
  */
 
 export default function App() {
+  function generateAllNewDice() {
+    let numbers = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
+      numbers.push(randomNumber);
+    }
+    return numbers;
+  }
+
+  console.log(generateAllNewDice());
+
   return (
     <main>
       <div className="dice-container">
